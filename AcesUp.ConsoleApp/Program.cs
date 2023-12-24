b0c37bc21feacd18ce22a9d45020385a98644e17
Christian Wibowo <christianbwibowo@gmail.com>
2023-12-24 11:37:05 +0800

    run the game 10000 times and record the winrate

--- AcesUp.ConsoleApp/Program.cs
+++ AcesUp.ConsoleApp/Program.cs
@@ -9,29 +9,44 @@ namespace AcesUp.ConsoleApp
             // Setting output encoding so the suit of the cards can be displayed in the console.
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            var r = new Random();
-
-            var deck = Deck.CreateShuffledDeck(r);
-
-            var game = new Game();
-
-            var round = 1;
-
-            while (!deck.IsEmpty)
+            int win = 0;
+            int match = 10000;
+            int degreeOfParallelism = -1;
+            Parallel.For(0, match, new ParallelOptions { MaxDegreeOfParallelism = degreeOfParallelism}, (_, _) =>
             {
-                game.RunAllSteps(deck);
+                var r = new Random();
 
-                Console.WriteLine($"Content of the piles after round {round++}:");
-                game.PrintPiles();
-                Console.WriteLine();
+                var deck = Deck.CreateShuffledDeck(r);
 
-            }
+                var game = new Game();
 
-            if (game.IsGameWon())
-            {
-                Console.WriteLine("Hooray - you won!");
-            }
+                var round = 1;
 
+                while (!deck.IsEmpty)
+                {
+                    game.RunAllSteps(deck);
+
+                    // Only print when running the match(es) in series.
+                    if (degreeOfParallelism == 1)
+                    {
+                        Console.WriteLine($"Content of the piles after round {round++}:");
+                        game.PrintPiles();
+                        Console.WriteLine();
+                    }
+                }
+
+                if (game.IsGameWon())
+                {
+                    win = Interlocked.Increment(ref win);
+
+                    if (degreeOfParallelism == 1)
+                    {
+                        Console.WriteLine("Hooray - you won!");
+                    }
+                }
+            });
+
+            Console.WriteLine($"Match: {match} - WIN/LOSE: {win}/{match-win}");
             Console.ReadLine();
         }
     }
